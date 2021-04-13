5da402c32d187d73a0eef4597d347f27052213f6
Oliver Eriksson <olieri@kth.se>
2021-04-13 11:15:30 +0200

    Create FPS camera and position cube in center of scene

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,7 @@ struct Camera {
 
 	glm::mat4 get_view_projection() const {
     
-    glm::mat4 rotate = glm::rotate(glm::identity<glm::mat4>(), 
-      glm::radians<float>(this->yaw), glm::vec3(0.0, 1.0, 0.0)
-    );
-    rotate = glm::rotate(rotate, glm::radians<float>(this->pitch), glm::vec3(1.0, 0.0, 0.0));
+    glm::mat4 rotate = get_rotation();
 
     glm::mat4 translate = glm::translate(glm::identity<glm::mat4>(), position);
 
@@ -26,12 +23,11 @@ struct Camera {
 		return projection * glm::inverse(translate * rotate);
 	}
 
-  glm::vec3 get_direction() const {
-    glm::vec3 direction;
-    direction.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
-    direction.y = sin(glm::radians(this->pitch));
-    direction.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
-    return direction;
+  glm::mat4 get_rotation() const {
+     glm::mat4 rotate = glm::rotate(glm::identity<glm::mat4>(), 
+      glm::radians<float>(this->yaw), glm::vec3(0.0, 1.0, 0.0)
+    );
+    return glm::rotate(rotate, glm::radians<float>(this->pitch), glm::vec3(1.0, 0.0, 0.0));
   }
 };
 
@@ -115,9 +111,10 @@ private:
 /** FUNCTIONS */
 void update(const Window& window, double dt, Camera& camera);
 GLuint create_shader_program();
+GLuint create_model_vao(float* data, size_t data_size, uint32_t* indices, size_t indices_size);
 
 const char* vertex_shader_code = R"(
-#version 410
+#version 410 core
 layout(location = 0) in vec3 a_Pos;
 
 uniform mat4 u_ViewProjection;
@@ -128,7 +125,7 @@ void main()
 )";
 
 const char* fragment_shader_code = R"(
-#version 410
+#version 410 core
 out vec4 color;
 
 void main()
@@ -142,15 +139,18 @@ int main(void)
   Window window;
   Clock clock;
 
+  GLuint shader_program = create_shader_program();
+  glUseProgram(shader_program);
+
   float cube_data[] = {
-    0.0f, 0.0f, 0.0f, // 0
-    1.0f, 0.0f, 0.0f, // 1
-    0.0f, 1.0f, 0.0f, // 2
-    1.0f, 1.0f, 0.0f, // 3
-    0.0f, 0.0f, 1.0f, // 4
-    1.0f, 0.0f, 1.0f, // 5
-    0.0f, 1.0f, 1.0f, // 6
-    1.0f, 1.0f, 1.0f, // 7
+    -0.5f, -0.5f, -0.5f, // 0
+    0.5f, -0.5f, -0.5f, // 1
+    -0.5f, 0.5f, -0.5f, // 2
+    0.5f, 0.5f, -0.5f, // 3
+    -0.5f, -0.5f, 0.5f, // 4
+    0.5f, -0.5f, 0.5f, // 5
+    -0.5f, 0.5f, 0.5f, // 6
+    0.5f, 0.5f, 0.5f, // 7
   };
 
   uint32_t cube_indices[] = {
@@ -167,34 +167,11 @@ int main(void)
     4, 0, 1, // Bottom
     1, 5, 4,
   };
+  GLuint cube_vao = create_model_vao(cube_data, sizeof(cube_data), cube_indices, sizeof(cube_indices));
 
-  GLuint shader_program = create_shader_program();
-  glUseProgram(shader_program);
-
-  GLuint water_vao;
-  glGenVertexArrays(1, &water_vao);
-
-  GLuint water_vbo;
-  glGenBuffers(1, &water_vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, water_vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(cube_data), cube_data, GL_STATIC_DRAW);
-
-  GLuint water_indices;
-  glGenBuffers(1, &water_indices);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, water_indices);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
-
-  glBindVertexArray(water_vao);
-  
-  glBindBuffer(GL_ARRAY_BUFFER, water_vbo);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, water_indices);
-  glUseProgram(shader_program);
-
-  Camera camera = { .position = glm::vec3(0.5, 0.5, 2.0),
-	  .yaw = 0.0, .pitch = 0.0,
-    .fovy = 90, 1260.0f / 1080.0f, 0.01, 1000.0
+  Camera camera = { .position = glm::vec3(0.0, 2.0, 5.0),
+	  .yaw = 0.0, .pitch = -20.0,
+    .fovy = 45.0f, 1260.0f / 1080.0f, 0.01, 1000.0
   };
 
   while (!window.should_close ())
@@ -202,14 +179,13 @@ int main(void)
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.8, 0.85, 1.0, 1.0);
 
-    update (window, clock.tick(), camera);
+    update(window, clock.tick(), camera);
     
+    glBindVertexArray(cube_vao);
     glm::mat4 view_projection = camera.get_view_projection();
     GLuint view_proj_loc = glGetUniformLocation(shader_program, "u_ViewProjection");
     glUniformMatrix4fv(view_proj_loc, 1, false, &view_projection[0][0]);
-
-    glDrawElements(GL_TRIANGLES, sizeof(cube_indices) / 3.0f, GL_UNSIGNED_INT, (void *) water_indices);
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, sizeof(cube_indices) / 3.0f, GL_UNSIGNED_INT, 0);
 
     window.swap_buffers ();
     window.poll_events ();
@@ -217,7 +193,7 @@ int main(void)
   return 0;
 }
 
-void update (const Window& window, double dt, Camera& camera) {
+void update(const Window& window, double dt, Camera& camera) {
   // std::cout << "Frame rate: " << 1.0 / dt << " FPS" << std::endl;
   if (window.is_key_pressed(GLFW_KEY_LEFT)) {
     camera.yaw += 100 * dt;
@@ -232,17 +208,20 @@ void update (const Window& window, double dt, Camera& camera) {
     camera.pitch -= 100 * dt;
   }
 
+  glm::vec3 forward = glm::vec3(camera.get_rotation() * glm::vec4(0.0, 0.0, -1.0, 0.0));
+  glm::vec3 right = glm::vec3(camera.get_rotation() * glm::vec4(1.0, 0.0, 0.0, 0.0));
+
   if (window.is_key_pressed(GLFW_KEY_W)) {
-    camera.position.z -= 1 * dt;
+    camera.position += forward * (float) dt;
   }
   if (window.is_key_pressed(GLFW_KEY_S)) {
-    camera.position.z += 1 * dt;
-  }
-  if (window.is_key_pressed(GLFW_KEY_A)) {
-    camera.position.x += 1 * dt;
+    camera.position -= forward * (float) dt;
   }
   if (window.is_key_pressed(GLFW_KEY_D)) {
-    camera.position.x -= 1 * dt;
+    camera.position += right * (float) dt;
+  }
+  if (window.is_key_pressed(GLFW_KEY_A)) {
+    camera.position -= right * (float) dt;
   }
 }
 
@@ -256,7 +235,7 @@ GLuint create_shader_program() {
   glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
   if(!success) {
     glGetShaderInfoLog(vs, sizeof(infoLog), NULL, infoLog);
-    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    std::cout << "ERROR: Vertex shader compilation failed\n" << infoLog << std::endl;
   };
 
   GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
@@ -265,7 +244,7 @@ GLuint create_shader_program() {
   glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
   if(!success) {
     glGetShaderInfoLog(fs, sizeof(infoLog), NULL, infoLog);
-    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    std::cout << "ERROR: Fragment shader compilation failed\n" << infoLog << std::endl;
   };
 
   GLuint program = glCreateProgram();
@@ -275,9 +254,33 @@ GLuint create_shader_program() {
   glGetProgramiv(program, GL_LINK_STATUS, &success);
   if(!success) {
     glGetProgramInfoLog(program, sizeof(infoLog), NULL, infoLog);
-    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    std::cout << "ERROR: Shader program linkage failed\n" << infoLog << std::endl;
   }  
   glDeleteShader(vs);
   glDeleteShader(fs);
   return program;
 };
+
+GLuint create_model_vao(float* data, size_t data_size, uint32_t* indices, size_t indices_size) {
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, data_size, data, GL_STATIC_DRAW);
+
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_size, indices, GL_STATIC_DRAW);
+
+  glBindVertexArray(vao);
+  
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+  return vao;
+}
